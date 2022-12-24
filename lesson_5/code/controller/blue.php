a94fb81af31927411f127ce512cd970b0d7dd2f8
vitaliy <chertkov.vitaliy@gmail.com>
2022-12-24 16:39:55 +0300

    Л5

---
+++ lesson_5/code/controller/blue.php
@@ -0,0 +1,17 @@
+<?php
+
+function blue($messages){
+    $str = "";
+    foreach( $messages as   $key => $value   ) {
+      $name = $value['name'];
+      $msg = $value['msg'];
+
+      $str .=  '<div class="card text-white bg-primary mb-3" style="max-width: 18rem;">'.
+      '<div class="card-header">'.  $name  . '</div>' .
+      '<div class="card-body">'.
+      '<h5 class="card-title"></h5>'.
+        '<p class="card-text">' . $msg . '</p>'.
+      '</div></div>'  ;
+    }
+    return $str;
+}
--- lesson_5/code/index.php
+++ lesson_5/code/index.php
@@ -4,12 +4,11 @@ require_once "./data/db.php";
 $action = isset($_GET['action'])? $_GET['action']: 'blue' ;
 
 $FOLDER_CONTOLLER = './controller';
-$FOLDER_UTIL = './util';
 $FOLDER_HELPER = './helper';
-$folder = [$FOLDER_CONTOLLER, $FOLDER_UTIL, $FOLDER_HELPER];
+$folder = [$FOLDER_CONTOLLER, $FOLDER_HELPER];
 
 
-//Автолоудер autoloader
+//Аutoloader
 foreach( $folder as $key => $FOLDER) {
     $file = $FOLDER . "/" . $_GET['action'] . ".php"; 
     @include $file;
